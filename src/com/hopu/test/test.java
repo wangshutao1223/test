4ac14a3b4fdbed69979bfed086ef70481999fae7
wangshutao1223 <18103942923@163.com>
2019-12-23 19:54:40 +0800

    重新上传分支

--- src/com/hopu/test/test.java
+++ src/com/hopu/test/test.java
@@ -2,11 +2,7 @@ package com.hopu.test;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("hahaha");
-
-		System.out.println("aaa");
-
-		System.out.println("cccccx");
+	System.out.println("11111111111");
 	}
 
 }
